debc088c485e0bb55cc877ad0d38077c554c1b4a
unknown <bogdan.lyamzin.d@gmail.com>
2023-09-07 20:59:58 +0300

    finish lesson 6

---
+++ lesson-6/project-backend/routes/api/movies-router.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+import moviesController from "../../controllers/movies-controller.js";
+
+import * as movieSchemas from "../../models/Movie.js";
+
+import {validateBody} from "../../decorators/index.js";
+
+import {isValidId} from "../../middlewares/index.js";
+
+const movieAddValidate = validateBody(movieSchemas.movieAddSchema);
+const movieUpdateFavoriteValidate = validateBody(movieSchemas.movieUpdateFavoriteSchema);
+
+const moviesRouter = express.Router();
+
+moviesRouter.get("/", moviesController.getAll);
+
+moviesRouter.get("/:id", isValidId, moviesController.getById);
+
+moviesRouter.post("/", movieAddValidate, moviesController.add);
+
+moviesRouter.put("/:id", isValidId, movieAddValidate, moviesController.updateById);
+
+moviesRouter.patch("/:id/favorite", isValidId, movieUpdateFavoriteValidate, moviesController.updateById);
+
+moviesRouter.delete("/:id", isValidId, moviesController.deleteById);
+
+export default moviesRouter;
